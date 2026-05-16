09a4ec3e6342eaf8315f39d291b04c6d1306ff61
loonatuiq <107491408+loonatuiq@users.noreply.github.com>
2026-05-16 15:55:38 +0700

    Add Russian language change support to i18n

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -5,6 +5,7 @@ import pl from './locales/pl.json';
 import it from './locales/it.json';
 import de from './locales/de.json';
 import fr from './locales/fr.json';
+import ru from './locales/ru.json';
 
 const resources = {
   en: { translation: en },
@@ -12,9 +13,10 @@ const resources = {
   it: { translation: it },
   de: { translation: de },
   fr: { translation: fr },
+  ru: { translation: ru },
 };
 
-export type SupportedLang = 'en' | 'pl' | 'it' | 'de' | 'fr';
+export type SupportedLang = 'en' | 'pl' | 'it' | 'de' | 'fr'| 'ru';
 
 export async function initI18n(lang: SupportedLang) {
   await i18n.use(initReactI18next).init({
